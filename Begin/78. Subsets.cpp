539fdd67134593859c93804f7b26a9bf4cf7b390
cloud-zhoubingye <zhou_bingye@outlook.com>
2025-06-22 11:12:13 +0800

    add subsets

---
+++ Begin/78. Subsets.cpp
@@ -0,0 +1,32 @@
+// https://leetcode.com/problems/subsets/
+
+class Solution
+{
+private:
+    vector<vector<int>> subset{{}}; // 初始时包含一个空集[[],...]
+    void backtrack(const vector<int> &nums, int nums_begin_idx, vector<int> &visited, vector<int> &track)
+    {
+        for (int i = nums_begin_idx; i < nums.size(); ++i)
+        {
+            // 避免重复数字
+            if (find(visited.begin(), visited.end(), nums[i]) != visited.end())
+                continue;
+            // 回溯算法
+            visited.push_back(nums[i]);
+            track.push_back(nums[i]);
+            this->subset.push_back(track);          // 保存结果子集
+            backtrack(nums, i + 1, visited, track); // i+1确保子集不重复
+            visited.pop_back();
+            track.pop_back();
+        }
+    }
+
+public:
+    vector<vector<int>> subsets(vector<int> &nums)
+    {
+        vector<int> visited;
+        vector<int> track;
+        backtrack(nums, 0, visited, track);
+        return this->subset;
+    }
+};
