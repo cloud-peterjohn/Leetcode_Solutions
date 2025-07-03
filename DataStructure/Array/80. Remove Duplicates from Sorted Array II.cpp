19f6f71f030a587916bf058c089ea5f3420b1c36
cloud-zhoubingye <zhou_bingye@outlook.com>
2025-07-03 11:08:25 +0800

    add array 80

--- DataStructure/Array/80. Remove Duplicates from Sorted Array II.cpp
+++ DataStructure/Array/80. Remove Duplicates from Sorted Array II.cpp
@@ -1,2 +1,28 @@
 // https://leetcode.com/problems/remove-duplicates-from-sorted-array-ii/
 
+class Solution
+{
+public:
+    int removeDuplicates(vector<int> &nums)
+    {
+        int slow = 0, fast = 0;
+        while (fast < nums.size())
+        {
+            bool is_valid = true;
+            if (slow - 1 >= 0 && nums[slow - 1] == nums[fast])
+            {
+                if (slow - 2 >= 0 && nums[slow - 2] == nums[fast])
+                {
+                    is_valid = false;
+                }
+            }
+            if (is_valid)
+            {
+                nums[slow] = nums[fast];
+                ++slow;
+            }
+            ++fast;
+        }
+        return slow;
+    }
+};
